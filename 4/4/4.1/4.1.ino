85ff38af76a056d8521513b2e3f06d989c50b146
RogueQuincy <46093247+RogueQuincy@users.noreply.github.com>
2025-10-07 11:00:07 +0300

    4 lab
    Десятисегментный линейный индикатор. Пульсирующая шкала

---
+++ 4/4/4.1/4.1.ino
@@ -0,0 +1,13 @@
+int leds[] = {13,12,11,10,9,8,7,6,5,4};
+void setup() {
+   pinMode(leds [0],OUTPUT);
+   pinMode(leds [1],OUTPUT);
+   pinMode(leds [2],OUTPUT);
+   pinMode(leds [3],OUTPUT);
+   pinMode(leds [4],OUTPUT);
+   pinMode(leds [5],OUTPUT);
+   pinMode(leds [6],OUTPUT);
+   pinMode(leds [7],OUTPUT);
+   pinMode(leds [8],OUTPUT);
+   pinMode(leds [9],OUTPUT);
+}
---
+++ 4/4/4.ino
@@ -0,0 +1,20 @@
+#define LED1      13
+#define LED2      12
+#define LED3      11
+#define LED4      10
+#define LED5      9
+#define LED6      8
+#define LED7      7
+#define LED7      6
+#define LED8      5
+#define LED8      4
+void setup() {
+   pinMode(LED1,OUTPUT);
+   pinMode(LED2,OUTPUT);
+   pinMode(LED3,OUTPUT);
+   pinMode(LED4,OUTPUT);
+   pinMode(LED5,OUTPUT);
+   pinMode(LED6,OUTPUT);
+   pinMode(LED7,OUTPUT);
+   pinMode(LED8,OUTPUT);
+}
